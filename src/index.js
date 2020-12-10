6e4a19b490324fb13d5ddf639c7dd3d8a32b0182
Igor Veyner <igorveyner95@gmail.com>
2020-12-10 14:33:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submit = document.querySelectorAll("input")[1];
+  const ul = document.querySelector("#tasks")
+  
+  submit.addEventListener("click", function(event) {
+    let text = document.getElementById("new-task-description")
+
+    event.preventDefault();
+    ul.innerHTML += `<li>${text.value}</li>`
+    text.value = ""
+  })
 });
