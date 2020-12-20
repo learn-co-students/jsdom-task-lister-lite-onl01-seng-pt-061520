cfc53672773c3b0a8b25fe8f486795582738b525
Adam Priest <adampriest@pop-os.localdomain>
2020-12-19 20:35:44 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let inputTag = document.getElementById('new-task-description');
+    let inputValue = inputTag.value;
+
+    let li = document.createElement('li');
+    li.innerText = inputValue;
+
+    let ul = document.getElementById('tasks');
+    ul.append(li);
+    inputTag.value;
+    
+  })
 });
