50e283a145024d98b61004c215c8a900880578ca
zach6585 <zmines@wesleyan.edu>
2020-12-28 15:26:54 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    let taskResponse = document.getElementById("new-task-description");
+    let ul = document.getElementById("tasks")
+    let li = document.createElement('li');
+    li.innerHTML = taskResponse.value;
+    ul.appendChild(li);
+    event.preventDefault();
+    
+}, false);
 });
