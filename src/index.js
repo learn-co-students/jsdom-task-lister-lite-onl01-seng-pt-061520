aca134371a3cdee2e305def3ff09735af20ddf60
Lukasz Kowalik <kowalix007@gmail.com>
2021-01-11 18:52:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  function formSubmit(e){
+    e.preventDefault();
+    const listItem = document.createElement("li");
+    const textValue = document.getElementById("new-task-description").value;
+    listItem.textContent = textValue;
+   
+    document.getElementById("tasks").append(listItem);
+  }
+  const form = document.getElementById('create-task-form');
+  const log = document.getElementById('new-task-description');
+  form.addEventListener('submit', formSubmit);
 });
