a561719776e8bb33b4c357606b877af85d1316ed
jpv2018 <jason.viola@mavs.uta.edu>
2021-01-18 23:38:23 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskList = document.querySelector('#tasks');
+  const taskInput = document.querySelector('#new-task-description');
+  const myForm = document.querySelector('#create-task-form');
+  myForm.addEventListener('submit', onSubmit);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    if(taskInput.value === '') {
+      alert('Please enter a task');
+    } else {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(`${taskInput.value}`));
+      console.log('success');
+      taskList.appendChild(li);
+      // Clear fields
+      taskInput.value = '';
+    }
+
+    console.log(taskInput.value);
+
+  }
+})
